539715fa5a55cc7e09609f7348c98cd74b230558
mikk7867 <56957585+mikk7867@users.noreply.github.com>
2024-09-19 12:59:01 +0200

    commit

---
+++ BlazorApp1Test/BlazorCodeTest.cs
@@ -0,0 +1,70 @@
+﻿using BlazorApp1.Components.Pages;
+using Bunit.TestDoubles;
+using Bunit;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+// test ikke logget ind, logget ind, og logget ind som admin
+
+namespace BlazorApp1Test;
+
+public class BlazorCodeTest
+{
+    [Fact]
+    public void TestNotLogedIn()
+    {
+        // Arrange
+        using var ctx = new TestContext();
+        var auth = ctx.AddTestAuthorization();
+        auth.SetNotAuthorized();
+        var cut = ctx.RenderComponent<Home>();
+
+        // Act
+        var authStatus = cut.Instance.AuthStatus;
+        var adminStatus = cut.Instance.AdminStatus;
+
+        // Assert
+        Assert.False(authStatus);
+        Assert.False(adminStatus);
+    }
+
+    [Fact]
+    public void TestLogedIn()
+    {
+        // Arrange
+        using var ctx = new TestContext();
+        var auth = ctx.AddTestAuthorization();
+        auth.SetAuthorized("test@test.test", AuthorizationState.Authorized);
+        var cut = ctx.RenderComponent<Home>();
+
+        // Act
+        var authStatus = cut.Instance.AuthStatus;
+        var adminStatus = cut.Instance.AdminStatus;
+
+        // Assert
+        Assert.True(authStatus);
+        Assert.False(adminStatus);
+    }
+
+    [Fact]
+    public void TestLogedInAdmin()
+    {
+        // Arrange
+        using var ctx = new TestContext();
+        var auth = ctx.AddTestAuthorization();
+        auth.SetRoles("Admin");
+        auth.SetAuthorized("test@test.test", AuthorizationState.Authorized);
+        var cut = ctx.RenderComponent<Home>();
+
+        // Act
+        var authStatus = cut.Instance.AuthStatus;
+        var adminStatus = cut.Instance.AdminStatus;
+
+        // Assert
+        Assert.True(authStatus);
+        Assert.True(adminStatus);
+    }
+}
---
+++ BlazorApp1Test/BlazorViewTest.cs
@@ -0,0 +1,62 @@
+﻿using Bunit;
+using BlazorApp1;
+using BlazorApp1.Components.Pages;
+using Bunit.TestDoubles;
+
+// test ikke logget ind, logget ind, og logget ind som admin
+
+namespace BlazorApp1Test;
+
+public class BlazorViewTest
+{
+    [Fact]
+    public void TestNotLogedIn()
+    {
+        // Arrange
+        using var ctx = new TestContext();
+        var auth = ctx.AddTestAuthorization();
+        auth.SetNotAuthorized();
+        var cut = ctx.RenderComponent<Home>();
+
+        // Act
+        var paraElm = cut.Find("p");
+
+        // Assert
+        var paraElmText = paraElm.TextContent;
+        paraElmText.MarkupMatches("You are NOT logged in!");
+    }
+
+    [Fact]
+    public void TestLogedIn()
+    {
+        // Arrange
+        using var ctx = new TestContext();
+        var auth = ctx.AddTestAuthorization();
+        auth.SetAuthorized("test@test.test", AuthorizationState.Authorized);
+        var cut = ctx.RenderComponent<Home>();
+
+        // Act
+        var paraElm = cut.Find("p");
+
+        // Assert
+        var paraElmText = paraElm.TextContent;
+        paraElmText.MarkupMatches("You are logged in!");
+    }
+
+    [Fact]
+    public void TestLogedInAdmin()
+    {
+        // Arrange
+        using var ctx = new TestContext();
+        var auth = ctx.AddTestAuthorization();
+        auth.SetRoles("Admin");
+        auth.SetAuthorized("test@test.test", AuthorizationState.Authorized);
+        var cut = ctx.RenderComponent<Home>();
+
+        // Act
+        var paraElms = cut.FindAll("p");
+
+        // Assert
+        Assert.Contains(paraElms, p => p.TextContent == "You are admin!");
+    }
+}
